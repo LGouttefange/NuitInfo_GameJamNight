ddbc5919f5653fd0c6903520bd6d80ab690aea39
Loic Gouttefangeas <loic.gouttefangeas.pro@gmail.com>
2016-12-01 23:09:28 +0100

    Added background image transition

--- sorcery.js
+++ sorcery.js
@@ -13,8 +13,28 @@ $( function() {
 	$(".section > action[name='reset']").on("doAction", resetGame);
 	$(".section > action[name='start']").on("doAction", startGame);
 
+	function pathOfImage(file_name) {
+		return "img/" + file_name;
+	}
+
+	function backgroundShouldBeReplaced(file_name) {
+		return $("#background-image").find("> img").attr("src") !== pathOfImage(file_name);
+	}
+
 	function setBackgroundImage(file_name) {
-		$("#background-image").css("background-image", "url(img/" + (file_name || "placeholder.png") + ")");
+
+		//$("#background-image").css("background-image", "url(img/" + (file_name || "placeholder.png") + ")");
+
+		if (backgroundShouldBeReplaced(file_name)) {
+
+			$("#background-image").find("> img")
+				.fadeOut(800, function () {
+					$(this).attr("src", "img/" + (file_name || "placeholder.png"));
+				})
+				.fadeIn(800);
+		}
+
+
 	}
 	
 	function gotoSection(key) {
@@ -32,10 +52,7 @@ $( function() {
 	function resetGame() {
 		gotoSection("wakeUp");
 	}
-	
-	function endGame() {
-		gotoSection("death");
-	}
+
 
 	$(".section +.section").hide();
 
