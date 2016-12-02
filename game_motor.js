ef5fd8ff74dd3af04af9c6220f9a51ae03d7a859
Loic Gouttefangeas <loic.gouttefangeas.pro@gmail.com>
2016-12-02 04:39:31 +0100

    Added sound effects and cool stuff

--- game_motor.js
+++ game_motor.js
@@ -1,12 +1,28 @@
 $.wait = function (callback, seconds) {
 	return window.setTimeout(callback, seconds * 1000);
+};
+var audioElement = document.createElement('audio');
+
+function addAudio() {
+	audioElement = document.createElement('audio');
+	audioElement.setAttribute('autoplay', 'autoplay');
+	//audioElement.load()
+	$.get();
+	audioElement.addEventListener("load", function () {
+		audioElement.play();
+	}, true);
+
+
+
 }
 $( function() {
-
+	addAudio();
 	var buttons = $(".section button");
 	var status = $("#status");
 	var currentSection = $(".section").first();
 	var $characterSprite = $("#sprite");
+
+
 
 
 	function sectionHasSprite() {
@@ -16,6 +32,14 @@ $( function() {
 	function pathOfSprite(name) {
 		return "img/sprite/" + name + ".png";
 	}
+
+	$("#input_name").blur(function(){
+		$("name").text($(this).val() );
+	});
+
+	$("#input_loveInterest").blur(function(){
+		$("loveInterest").text($(this).val() );
+	});
 
 	function tryShowSprite(sprite_name) {
 		if (sectionHasSprite())
@@ -30,10 +54,31 @@ $( function() {
 		;
 	}
 
+	function pathOfSoundFX(file_name) {
+		return "soundfx/" + file_name + ".mp3";
+	}
+
+	function applySoundFX(currentSection) {
+		audioElement.pause();
+		if( currentSection.find('soundfx').length >  0 ){
+			$(audioElement).attr('src', pathOfSoundFX(currentSection.find('soundfx').text()));
+			audioElement.play();
+		}
+	}
+
 	buttons.click( function() {
 		gotoSection($(this).attr("go"));
 	} );
 
+	$(".section > action[name='setGender']").on("doAction", setGender);
+
+	function setGender(){
+		if( $(this).data('gender') === "male" )
+			$('fille').hide();
+		else
+			$('garcon').hide();
+
+	}
 
 	function pathOfImage(file_name) {
 		return "img/" + file_name + ".jpg";
@@ -74,6 +119,7 @@ $( function() {
 	function gotoSection(key) {
 		currentSection.hide();
 		currentSection = $("#" + key);
+		applySoundFX(currentSection);
 
 		setBackgroundImage(currentSection.find("img_src").text().trim());
 		swapSprites(currentSection.find("sprite").text());
@@ -85,6 +131,6 @@ $( function() {
 
 
 	$(".section +.section").hide();
-
+	addAudio();
 
 } );
